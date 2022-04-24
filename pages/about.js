e20bae405d88cf489359b24f710771abb338d5be
Kataoka Yuto <konamonhyogo@gmail.com>
2022-04-24 16:24:10 +0900

    jest,storybook追加

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,7 @@
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-
 export default function About() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Go Back</a>
-          </Link>
-        </p>
-      </main>
+    <div>
+      <h1>About Page</h1>
     </div>
-  )
+  );
 }
